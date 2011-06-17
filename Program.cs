c48698d7805f7291e45a112066d045ee0972c569
Paul Campbell <paulgcampbell@gmail.com>
2011-06-17 14:54:02 +0100

    it talks. Mwah hahahahah

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace BotTask
         static void Main(string[] args)
         {
 
+
+            // publish arrivals
            var arrivals = GetArrivals();
 
             foreach (var a in  arrivals)
@@ -21,12 +23,13 @@ namespace BotTask
                 var arrivaltime = DateTime.Parse(a.date + " " + a.scheduled_arrival_time);
                 if ((arrivaltime < DateTime.Now) && arrivaltime >  DateTime.Now.AddMinutes(-10) )
                 {
-                    tweet(string.Format("Flight {0} from {1} has landed.", a.flight_number, a.from));
+                    Tweet(string.Format("Flight {0} from {1} has landed.", a.flight_number, a.from));
 
                 }
             }
 
 
+            //publish departures
             var departures = GetDepartures();
 
             foreach (var d in departures)
@@ -34,12 +37,79 @@ namespace BotTask
                 var arrivaltime = DateTime.Parse(d.date + " " + d.scheduled_departure_time);
                 if ((arrivaltime < DateTime.Now) && arrivaltime > DateTime.Now.AddMinutes(-10))
                 {
-                    tweet(string.Format("Flight {0} from {1} has taken off.", d.flight_number, d.destination));
+                    Tweet(string.Format("Flight {0} from {1} has taken off.", d.flight_number, d.destination));
 
                 }
             }
+
+            //respond to tweets
+            OAuthTokens tokens = new OAuthTokens();
+            tokens.AccessToken = System.Configuration.ConfigurationManager.AppSettings["AccessToken"];
+            tokens.AccessTokenSecret = System.Configuration.ConfigurationManager.AppSettings["AccessTokenSecret"];
+            tokens.ConsumerKey = System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"];
+            tokens.ConsumerSecret = System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"];
+
+            string query = "@robinhoodbot";
+
+            SearchOptions options = new SearchOptions()
+            {
+                PageNumber = 1,
+                NumberPerPage = 50
+            };
+
+            TwitterResponse<TwitterSearchResultCollection> searchResult = TwitterSearch.Search(query, options);
+
+                foreach (var tweet in searchResult.ResponseObject)
+                {
+                
+                    RespondToTweet(tweet);
+
+                }
+
+            
         }
       
+        public static void RespondToTweet(TwitterSearchResult tweet)
+        {
+            if (tweet.CreatedDate > DateTime.Now.AddMinutes(-10))
+            {
+
+                foreach (var a in GetArrivals())
+                {
+                    var tweetWords = tweet.Text.Split(" ".ToCharArray());
+                    foreach (var w in tweetWords.Where(w => (w == a.flight_number) || (w == a.from)))
+                    {
+                        var arrivaltime = DateTime.Parse(a.date + " " + a.scheduled_arrival_time);
+                        var bodyText = "will be arriving";
+                        if (arrivaltime < DateTime.Now)
+                        {
+                            bodyText = "arrived";
+                        }
+
+                        Tweet(string.Format("@{4}: Flight {0} from {3} {2} at {1}.", a.flight_number, a.scheduled_arrival_time, bodyText, a.from, tweet.FromUserScreenName));
+                    }
+                }
+
+
+
+                foreach (var d in GetDepartures())
+                {
+                    var tweetWords = tweet.Text.Split(" ".ToCharArray());
+                    foreach (var w in tweetWords.Where(w => (w == d.flight_number) || (w == d.destination)))
+                    {
+                        var arrivaltime = DateTime.Parse(d.date + " " + d.scheduled_departure_time);
+                        var bodyText = "will be leaving";
+                        if (arrivaltime < DateTime.Now)
+                        {
+                            bodyText = "left";
+                        }
+
+                        Tweet(string.Format("@{4}: Flight {0} to {3} {2} at {1}.", d.flight_number, d.scheduled_departure_time, bodyText, d.destination, tweet.FromUserScreenName));
+                    }
+                }
+
+            }
+        }
 
         public static IList<arrival> GetArrivals()
         {
@@ -86,7 +156,7 @@ namespace BotTask
             return departures;
         }
 
-        public static void tweet(string tweet)
+        public static void Tweet(string tweet)
         {
           
             OAuthTokens tokens = new OAuthTokens();
